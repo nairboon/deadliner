d32de8b68f0b04fe2a8b912c2ce475f0ffafb5bd
Remo Hertig <nairboon@fsfe.org>
2023-04-05 13:37:12 +0200

    exam day bugfix

--- Changelog.md
+++ Changelog.md
@@ -1,6 +1,10 @@
  
 # Changelog
 
+## [0.0.2] - 2023-04-05
+
+- fix current exam day
+
 
 ## [0.0.1] - 2022-12-20
 
--- deadliner/__init__.py
+++ deadliner/__init__.py
@@ -202,7 +202,7 @@ class DeadlinerDialog(QDialog):
 
     def onToggleEnable(self, enabled):
         self.deadlineDeck.enabled = enabled
-        self.gbox.setDisabled(enabled)
+        self.gbox.setDisabled(not enabled)
         # if enabled:
         #     showInfo("Enabled")
         #     #self.enabledButton.setChecked(True)
@@ -255,8 +255,11 @@ class DeadlineStats:
 
         mature, young, new, _ = self.count_cards()
         print(f"mature: {mature}, young: {young}, new: {new}")
-
-        self.progress = mature / (mature + young + new)
+        denom = mature + young + new
+        if denom > 0:
+            self.progress = mature / (mature + young + new)
+        else:
+            self.progress = 0
 
         todo = young + new
         total_reps = todo * avg_reps
@@ -264,8 +267,13 @@ class DeadlineStats:
         print(f"need for {todo} at least {int(total_reps)} reps (each with {avg_sec:3.2f}s which takes {total_time/60:.1f}min")
 
         self.todoLearnN = todo
-        self.todoReps = int(total_reps/self.daysLeft)
-        self.todoTime = total_time/self.daysLeft/60 #min
+        if self.daysLeft != 0:
+            self.todoReps = int(total_reps/self.daysLeft)
+            self.todoTime = total_time/self.daysLeft/60 #min
+        else:
+            self.todoReps = int(total_reps)
+            self.todoTime = total_time/60 #min
+            
         print(f"Per day: reps: {self.todoReps} time: {self.todoTime} min")
 
     def get_day_stats(self):
@@ -352,7 +360,12 @@ def display_footer(deck_browser, content):
     has_deadlines = len(deadlines) > 0
 
     res = """<br><br><link rel="stylesheet" type="text/css" href="{}/styles.css"/><h3 style="display: inline-block;margin: 0 20px" >Deadlines</h3>
-    <div class="settings-btn" onclick='pycmd(\"add-new-event\");'></div>""".format(
+    <div class="settings-btn" onclick='pycmd(\"refreshDeadliner\");'>
+    <!--
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width=24 height=24>
+    <path d="M142.9 142.9c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5c0 0 0 0 0 0H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5c7.7-21.8 20.2-42.3 37.8-59.8zM16 312v7.6 .7V440c0 9.7 5.8 18.5 14.8 22.2s19.3 1.7 26.2-5.2l41.6-41.6c87.6 86.5 228.7 86.2 315.8-1c24.4-24.4 42.1-53.1 52.9-83.7c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.2 62.2-162.7 62.5-225.3 1L185 329c6.9-6.9 8.9-17.2 5.2-26.2s-12.5-14.8-22.2-14.8H48.4h-.7H40c-13.3 0-24 10.7-24 24z"/>
+    </svg> -->
+    </div>""".format(
         base_url, base_url)
 
     if has_deadlines:
